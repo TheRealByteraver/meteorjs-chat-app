d13c65c36b434ff9fa58ca5f0253ed6201b5e648
Erland <erland.vo@telenet.be>
2021-11-20 20:06:59 +0100

    added logout button, basic functionality is there

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -8,18 +8,39 @@ import './Login.js';
 const getUser = () => Meteor.user();
 const isUserLogged = () => !!getUser(); // force boolean
 
+Template.mainContainer.events({
+  'click .logout'(){
+    Meteor.logout();
+  }
+});
+
 Template.mainContainer.helpers({
   chatRooms() {
-    // get all chatRooms
-    const chatRooms = ChatRoomsCollection.find({});
+    // get all chatRooms in which this user is taking part
+    const currentUser = getUser();
+    const chatRooms = ChatRoomsCollection.find(
+      // filter: only select chatRooms where this user is taking part
+      { userIdList: 
+        { 
+          "userId" : currentUser._id, 
+          "username" : currentUser.username 
+        } 
+      },
+      // oldest first
+      { sort: { createdAt: 1 } }
+    );
 
     // find all messages for this chatRoom based on the _id
     // of this chatRoom and remap the messages to an array 
     // of objects of the following format:
-    // { author: username, message: 'message text' }
-    return chatRooms.map(chatRoom => {
-
-      // only keep the messages for this particular chatRoom
+    // { 
+    //   _id,              // Mongo Id of the message
+    //   disabledAttr,     // undefined or equal to 'disabled'
+    //   author,           // username, empty if 'own' message
+    //   message: 'message text' 
+    // }
+    const getMessages = (chatRoom) => {
+      // Select the messages for this particular chatRoom
       const messageFilter = { chatRoomId: chatRoom._id };
 
       // sort: oldest messages first
@@ -28,24 +49,51 @@ Template.mainContainer.helpers({
       const messages = ChatMessagesCollection.find(
         messageFilter, messageSort).fetch();
 
-      // set it to undefined or 'disabled'
-      // const disabledAttr = 'disabled';
-      const disabledAttr = undefined; // TEMP
+      return messages.map(message => {
+        const author = (message.author.username === currentUser.username) 
+          ? ''
+          : `${message.author.username} says:`;
 
-      return {
-        ...chatRoom,
-        messages: messages.map(message => ({
+        return {
           // keep track of the Mongo _id so we can manipulate the message elsewhere
           _id: message._id, 
-          disabledAttr,
-          author: message.author.username,
+          // disable message delete button for messages that are not from the
+          // current User:
+          disabledAttr: (message.author.username === currentUser.username) 
+            ? undefined 
+            : 'disabled',
+          author,
           message: message.message
-        }))
+        };
+      });
+    }
+
+    const getTalkingToStr = (chatRoom) => (
+      chatRoom.userIdList
+        .map(user => user.username)
+        .filter(username => (username !== currentUser.username))
+        .join(', '));
+
+    return chatRooms.map(chatRoom => {
+
+      // const messages = getMessages(chatRoom);
+
+      return {
+        ...chatRoom,   
+        talkingTo: getTalkingToStr(chatRoom),
+        messages: getMessages(chatRoom),
       };
     });
   },
   isUserLogged() {
     return isUserLogged();
+  },
+  getUsername() {
+    const user = getUser();
+    // console.log('user: ', user);
+    return user
+      ? ` ${user.username}`
+      : ''; 
   },
 });
 
@@ -61,11 +109,13 @@ Template.form.events({
     // Insert a task into the collection
     // Meteor.call('tasks.insert', text);
     const chatRoomId = target.previousElementSibling.dataset.chatroomindex;
+    const currentUser = getUser();
+
     ChatMessagesCollection.insert({
       chatRoomId,
       author: {
-        userId: 'unknown',   // TEMP
-        username: 'unknown', // TEMP
+        userId: currentUser._id,   
+        username: currentUser.username, 
       },
         createdAt: new Date(), 
         message: text
